6f712ae5304124398da215aed0f9ed03624828d4
Rejiejay <454766952@qq.com>
2018-09-26 14:32:41 +0800

    update login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,4 +1,4 @@
-<!-- 商家列表 -->
+<!-- 登录页 -->
 <template>
 <div class="login">
 
@@ -27,6 +27,32 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <!-- 协议 -->
+    <div class="login-agreement">
+        <div class="login-agreement-content flex-start-center" @click="isAgreement = !isAgreement">
+            <div class="agreement-radio-content" 
+                :class="[isAgreement ? 'agreement-radio-selected' : 'login-agreement-radio']"
+            >
+                <svg width="16" height="16" t="1535773287663" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2604" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <path d="M511.452 957.752c-246.502 0-447.037-200.535-447.037-447.027 0-246.497 200.535-447.037 447.037-447.037S958.49 264.228 958.49 510.725c0 246.492-200.535 447.027-447.038 447.027z m0-842.788c-218.224 0-395.766 177.542-395.766 395.766s177.542 395.75 395.766 395.75 395.766-177.525 395.766-395.75-177.541-395.766-395.766-395.766z" p-id="2605"></path>
+                    <path v-if="isAgreement" d="M438.477 684.841a25.605 25.605 0 0 1-18.125-7.516l-148.46-148.47a25.626 25.626 0 0 1 0-36.25 25.626 25.626 0 0 1 36.25 0l130.335 130.33 272.44-272.42a25.626 25.626 0 0 1 36.25 0 25.626 25.626 0 0 1 0 36.25L456.602 677.32a25.641 25.641 0 0 1-18.125 7.521z" p-id="2606"></path>
+                </svg>
+            </div>
+
+            <div class="login-agreement-text">
+                我已阅读并同意<span @click="jumpToAgreement">《养车频道用户服务协议》</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- 提交 -->
+    <div class="login-submit" @click="submitLogin">
+        <div class="login-submit-content">登录</div>
+    </div>
+    <div class="register-submit" @click="submitRegister">
+        <div class="register-submit-content">注册</div>
     </div>
 
 </div>
@@ -63,6 +89,9 @@ export default {
 			isMachineModalShow: false,
             
 			machineNumberErrorMsg: '', // 人机验证码错误信息
+			
+			// 是否同意协议
+            isAgreement: false,
         }
     },
 
@@ -102,6 +131,25 @@ export default {
 				this.isMachineModalShow = true; // 弹出模态框
 			}
 		},
+
+		/**
+		 * 跳转到 养车频道用户服务协议
+		 */
+		jumpToAgreement: function jumpToAgreement() {
+			this.$router.push({ path: '/agreement' });
+        },
+
+		/**
+		 * 登录
+		 */
+		submitLogin: function submitLogin() {
+        },
+
+		/**
+		 * 注册
+		 */
+		submitRegister: function submitRegister() {
+        },
 	},
 }
 
@@ -116,7 +164,7 @@ export default {
 .login {
     width: 100%;
     min-height: 100%;
-    background: #f5f5f5;
+    background: #F1F1F1;
 }
 
 // 输入
@@ -184,6 +232,73 @@ export default {
         background-color: #ddd;
         height: 1px;
         width: 100%;
+    }
+}
+
+// 协议
+.login-agreement {
+    padding: 0px 15px;
+
+    .login-agreement-content {
+        height: 35px;
+
+        // 单选框
+        .agreement-radio-content {
+            position: relative;
+            top: 1px;
+            width: 15px;
+        }
+    }
+
+    // 单选框
+    .login-agreement-radio path {
+        fill: @black3;
+    }
+    
+    // 单选框选中
+    .agreement-radio-selected path {
+        fill: #E50012;
+    }
+
+    // 协议文本
+    .login-agreement-text {
+        padding-left: 5px;
+        font-size: 12px;
+        color: @black3;
+
+        span {
+            color: #5594FF;
+        }
+    }
+}
+
+// 登录
+.login-submit {
+    padding: 10px 15px;
+    
+    .login-submit-content {
+        height: 45px;
+        line-height: 45px;
+        font-size: 16px;
+        border-radius: 3px;
+        text-align: center;
+        color: #fff;
+        background-color: #E50012;
+    }
+}
+
+// 注册
+.register-submit {
+    padding: 5px 15px 10px;
+    
+    .register-submit-content {
+        height: 45px;
+        line-height: 45px;
+        font-size: 16px;
+        border-radius: 3px;
+        text-align: center;
+        color: #E50012;
+        background-color: #fff;
     }
 }
 

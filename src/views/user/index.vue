0201280435fb34049ecf0307a067cd3b6be595ae
Rejiejay <454766952@qq.com>
2018-09-29 22:14:08 +0800

    add user-insurance

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -93,6 +93,63 @@
         </div>
     </div>
 
+    <!-- 保险出单 -->
+    <div class="user-proto-content user-insurance">
+        <div class="proto-content">
+
+            <div class="user-insurance-main flex-start-center">
+                <div class="user-insurance-left flex-rest">
+                    保险出单
+                </div>
+                <div class="user-insurance-right flex-start-center">
+                    <span>全部</span>
+                    <svg width="14" height="14" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="客户" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="客户管理" transform="translate(-696.000000, -280.000000)" fill="#AAAAAA" fill-rule="nonzero"><g id="客户1" transform="translate(0.000000, 226.000000)"><g id="Group" transform="translate(696.000000, 54.000000)">
+                        <path fill="#E50012" d="M12.2928932,2.70710678 C11.9023689,2.31658249 11.9023689,1.68341751 12.2928932,1.29289322 C12.6834175,0.902368927 13.3165825,0.902368927 13.7071068,1.29289322 L23.7071068,11.2928932 C24.0976311,11.6834175 24.0976311,12.3165825 23.7071068,12.7071068 L13.7071068,22.7071068 C13.3165825,23.0976311 12.6834175,23.0976311 12.2928932,22.7071068 C11.9023689,22.3165825 11.9023689,21.6834175 12.2928932,21.2928932 L21.5857864,12 L12.2928932,2.70710678 Z" id="Path-2"></path></g></g></g></g>
+                    </svg>
+                </div>
+            </div>
+
+            <div class="user-insurance-other flex-start-center">
+                
+                <div class="insurance-other-item flex-center">
+                    <div class="other-item-content flex-column-center">
+                        <div class="other-item-icon">
+                            <svg width="22" height="22" viewBox="0 0 44 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="我的" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="个人中心" transform="translate(-104.000000, -865.000000)" stroke="#444444"><g id="保险出单" transform="translate(20.000000, 744.000000)"><g id="已报价" transform="translate(84.000000, 121.000000)"><path d="M4,5 C2.34314575,5 1,6.34314575 1,8 L1,32 C1,33.6568542 2.34314575,35 4,35 L11,35 L11,41.8512053 C11,41.8808191 11.0087657,41.9097702 11.0251925,41.9344104 C11.0711453,42.0033397 11.1642757,42.0219658 11.233205,41.9760129 L21.6972244,35 L22,35 L40,35 C41.6568542,35 43,33.6568542 43,32 L43,8 C43,6.34314575 41.6568542,5 40,5 L4,5 Z" id="Rectangle-6" stroke-width="2"></path><path d="M37,18.5 L32.2071068,18.5 L37.3535534,13.3535534 C37.5488155,13.1582912 37.5488155,12.8417088 37.3535534,12.6464466 C37.1582912,12.4511845 36.8417088,12.4511845 36.6464466,12.6464466 L32,17.2928932 L27.3535534,12.6464466 C27.1582912,12.4511845 26.8417088,12.4511845 26.6464466,12.6464466 C26.4511845,12.8417088 26.4511845,13.1582912 26.6464466,13.3535534 L31.7928932,18.5 L27,18.5 C26.7238576,18.5 26.5,18.7238576 26.5,19 C26.5,19.2761424 26.7238576,19.5 27,19.5 L31.5,19.5 L31.5,22.5 L27,22.5 C26.7238576,22.5 26.5,22.7238576 26.5,23 C26.5,23.2761424 26.7238576,23.5 27,23.5 L31.5,23.5 L31.5,29 C31.5,29.2761424 31.7238576,29.5 32,29.5 C32.2761424,29.5 32.5,29.2761424 32.5,29 L32.5,23.5 L37,23.5 C37.2761424,23.5 37.5,23.2761424 37.5,23 C37.5,22.7238576 37.2761424,22.5 37,22.5 L32.5,22.5 L32.5,19.5 L37,19.5 C37.2761424,19.5 37.5,19.2761424 37.5,19 C37.5,18.7238576 37.2761424,18.5 37,18.5 Z M7,16.5 C6.72385763,16.5 6.5,16.7238576 6.5,17 C6.5,17.2761424 6.72385763,17.5 7,17.5 L15,17.5 C15.2761424,17.5 15.5,17.2761424 15.5,17 C15.5,16.7238576 15.2761424,16.5 15,16.5 L7,16.5 Z M7,22.5 C6.72385763,22.5 6.5,22.7238576 6.5,23 C6.5,23.2761424 6.72385763,23.5 7,23.5 L19,23.5 C19.2761424,23.5 19.5,23.2761424 19.5,23 C19.5,22.7238576 19.2761424,22.5 19,22.5 L7,22.5 Z" id="Combined-Shape"></path></g></g></g></g></svg>
+                        </div>
+                        <div class="other-item-describe">已报价</div>
+                    </div>
+                </div>
+                
+                <div class="insurance-other-item flex-center">
+                    <div class="other-item-content flex-column-center">
+                        <div class="other-item-icon">
+                            <svg width="22" height="22" viewBox="0 0 44 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="我的" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="个人中心" transform="translate(-270.000000, -865.000000)" stroke-width="2"><g id="保险出单" transform="translate(20.000000, 744.000000)"><g id="待支付" transform="translate(250.000000, 120.000000)"><path d="M4,9.92488586 L4,10 L37.1270953,10 L35.5791462,4.22297508 C35.5746324,4.20625817 35.5746324,4.20625817 35.5699901,4.18957649 C35.1227192,2.59423493 33.4668545,1.66353983 31.871513,2.1108108 L4,9.92488586 Z M3.3681859,10 L3.7320803,10 L3.26995191,10.1295626 L3.3681859,10 Z" id="Combined-Shape" stroke="#666666"></path><rect id="Rectangle-11-Copy" stroke="#444444" x="1" y="10" width="42" height="30" rx="4"></rect><path d="M43,30 L43,20 L32,20 C30.3431458,20 29,21.3431458 29,23 L29,27 C29,28.6568542 30.3431458,30 32,30 L43,30 Z" id="Rectangle-12" stroke="#444444"></path><circle id="Oval-2" stroke="#444444" cx="34" cy="25" r="1"></circle></g></g></g></g></svg>
+                        </div>
+                        <div class="other-item-describe">待支付</div>
+                    </div>
+                </div>
+                
+                <div class="insurance-other-item flex-center">
+                    <div class="other-item-content flex-column-center">
+                        <div class="other-item-icon">
+                            <svg width="22" height="22" viewBox="0 0 44 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="我的" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="个人中心" transform="translate(-436.000000, -865.000000)" stroke="#444444" stroke-width="2"><g id="保险出单" transform="translate(20.000000, 744.000000)"><g id="已承保" transform="translate(416.000000, 121.000000)"><path d="M22,42.9677261 C28.1122759,41.3642809 34.7639677,36.7814183 37.0636708,30.6488766 C39.1663217,25.0418075 39.8964454,18.9405899 39.9862356,9.98996878 C39.9920947,9.40591531 39.9960877,8.65163147 39.9982059,7.72752713 C39.9993391,7.23309839 39.6389754,6.81206463 39.1502924,6.73686908 C35.3147004,6.14662878 32.5012421,5.55429962 30.6884876,4.95025481 C28.7852356,4.3160545 26.0424072,3.11284011 22.4425408,1.33643875 C22.1635748,1.19877934 21.8364408,1.19878585 21.5574726,1.33646452 C17.9602759,3.11167886 15.2194567,4.31421191 13.3175698,4.94823594 C11.503484,5.55299006 8.68802128,6.14596223 4.84974753,6.73678669 C4.36105187,6.81201148 4.00069916,7.23310131 4.00188787,7.72755014 C4.00409708,8.64537322 4.00824458,9.39955311 4.01432151,9.98970329 C4.10635949,18.9278064 4.83999344,25.0586479 6.93632918,30.6488766 C9.23603234,36.7814183 15.8877241,41.3642809 22,42.9677261 Z" id="Rectangle-14"></path><path d="M23.2071068,20 L22.5,20 L23,20 L17,14 L22,19 L22.3535534,18.6464466 L21,20 L23.2071068,20 Z M22.3535534,18.6464466 L24.8535534,16.1464466 L22.3535534,18.6464466 Z M24.8535534,16.1464466 L27,14 L24.8535534,16.1464466 Z" id="Combined-Shape"></path></g></g></g></g></svg>
+                        </div>
+                        <div class="other-item-describe">已承保</div>
+                    </div>
+                </div>
+                
+                <div class="insurance-other-item flex-center">
+                    <div class="other-item-content flex-column-center">
+                        <div class="other-item-icon">
+                            <svg width="22" height="22" viewBox="0 0 44 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="我的" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="个人中心" transform="translate(-602.000000, -865.000000)" stroke="#444444" stroke-width="2"><g id="保险出单" transform="translate(20.000000, 744.000000)"><g id="已取消" transform="translate(582.000000, 121.000000)"><path d="M9,2 C7.34314575,2 6,3.34314575 6,5 L6,39 C6,40.6568542 7.34314575,42 9,42 L35,42 C36.6568542,42 38,40.6568542 38,39 L38,11.4012557 C38,11.1279726 37.8881566,10.8665955 37.6904757,10.6779002 L28.8891603,2.27664446 C28.703136,2.09907584 28.455853,2 28.1986845,2 L9,2 Z" id="Rectangle-17"></path><path d="M18.6464466,26.6464466 L22,30 L18.6464466,33.3535534 L22,30 L25.3535534,33.3535534 L22,30 L28,24 L22,30 L18.6464466,26.6464466 Z M18.6464466,33.3535534 L16,36 L18.6464466,33.3535534 Z M25.3535534,33.3535534 L28,36 L25.3535534,33.3535534 Z M13,12 L21,12 L13,12 Z M13,18 L31,18 L13,18 Z M34,12 L37,12 L36,11 L36,13 L37,12 L34,12 Z M34,12 L31,12 C29.3431458,12 28,10.6568542 28,9 L28,3 L27,4 L29,4 L28,3 L28,9 C28,10.6568542 29.3431458,12 31,12 L34,12 Z" id="Combined-Shape" fill-rule="nonzero"></path></g></g></g></g></svg>
+                        </div>
+                        <div class="other-item-describe">已取消</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- 底部 Tabbar -->
     <Tabbar selectTabbar='user'/>
 </div>
@@ -294,6 +351,38 @@ export default {
 
     .user-combo-center {
         padding-left: 5px;
+    }
+}
+
+// 保险出单
+.user-insurance {
+
+    .user-insurance-main {
+        min-height: 45px;
+        padding: 0px 15px;
+        border-bottom: 1px solid #ddd;
+
+        .user-insurance-left {
+            color: @black1;
+        }
+
+        .user-insurance-right {
+            color: #E50012;
+        }
+    }
+
+    .user-insurance-other {
+        padding: 15px;
+
+        .insurance-other-item {
+            width: 25%;
+        }
+
+        .other-item-describe {
+            padding-top: 2.5px;
+            font-size: 12px;
+            color: @black3;
+        }
     }
 }
 

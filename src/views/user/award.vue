ba396b947445baa14210895cd1a65708fe6c328b
Rejiejay <454766952@qq.com>
2018-10-08 17:13:20 +0800

    add award

--- src/views/user/award.vue
+++ src/views/user/award.vue
@@ -6,7 +6,7 @@
             <!-- 标题 -->
             <div class="user-award-title flex-start-center">
                 <div class="award-title-main flex-rest">我的奖励</div>
-                <div class="award-title-lable flex-start-center">累计:<span>¥99.98</span></div>
+                <div class="award-title-lable flex-start-center">累计:<span>¥{{amountsCostMoney}}</span></div>
             </div>
             <!-- 列表标题 -->
             <div class="user-award-item flex-start-center">
@@ -23,8 +23,8 @@
             >
                 <div class="user-award-item flex-start-center">
                     <div class="award-item-name">{{item.name}}</div>
-                    <div class="award-item-oil">{{item.sum}}</div>
-                    <div class="award-item-divide">{{item.sharing}}</div>
+                    <div class="award-item-oil">¥{{item.sum}}</div>
+                    <div class="award-item-divide">¥{{item.sharing}}</div>
                     <div class="award-item-time">{{item.time}}</div>
                 </div>
                 <div class="user-award-line" v-if="key !== (awardList.length - 1)"><div class="award-line-content"></div></div>
@@ -36,6 +36,8 @@
 
 <script>
 
+import ajaxs from "@/api/user/award";
+
 export default {
     name: 'user-award',
 
@@ -44,25 +46,50 @@ export default {
             clientWidth: document.body.offsetWidth || document.documentElement.clientWidth || window.innerWidth, // 设备的宽度
             clientHeight: document.body.offsetHeight || document.documentElement.clientHeight || window.innerHeight, // 设备高度
 
+            agentId: '8686253daa364003945ddae12b4ff75c', // 用户id
+
+            amountsCostMoney: 0, // 累计金额
+
             awardList: [ // 我的奖励
-                {
-                    name: '昵称', // 昵称
-                    sum: '300.00', // 加油金额
-                    sharing: '3.00', // 我的分成
-                    time: '2018-10-7', // 时间
-                }, {
-                    name: '昵称', // 昵称
-                    sum: '300.00', // 加油金额
-                    sharing: '3.00', // 我的分成
-                    time: '2018-10-7', // 时间
-                }, 
+                // {
+                //     name: '昵称', // 昵称
+                //     sum: '300.00', // 加油金额
+                //     sharing: '3.00', // 我的分成
+                //     time: '2018-10-7', // 时间
+                // },
             ],
         } 
     },
 
-	mounted: function mounted() { },
+	mounted: function mounted() {
+        this.getMyRewards();
+    },
 
-	methods: {}
+	methods: {
+        getMyRewards: function getMyRewards() {
+            const _this = this;
+
+            ajaxs.findMyRewards(this.agentId)
+            .then(
+                res => {
+                    let amountsCostMoney = 0;
+
+                    _this.awardList = res.map(val => {
+                        amountsCostMoney += parseInt(val.costMoney);
+
+                        return {
+                            name: val.userName, // 昵称
+                            sum: val.costMoney, // 加油金额
+                            sharing: val.obtainMoney.toFixed(2), // 我的分成
+                            time: val.recordDate, // 时间
+                        }
+                    });
+                }, error => {
+                    alert(error);
+                }
+            );
+        },
+    }
 }
 
 </script>
